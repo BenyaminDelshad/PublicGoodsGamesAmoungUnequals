02a72a1084379d17374982ce8f2b206f3e220abc
benyamindelshad <benyamindelshad@gmail.com>
2020-05-10 12:02:36 +0200

    Change definition of overall cooperation in threshold functions and use total amount of endowments which is contributed instead of summation of cooperation rates of players.

--- payoffPGG.m
+++ payoffPGG.m
@@ -35,10 +35,17 @@ for i=1:nOS
     s=OS(i,:); % s = (s1,s2,s3) contribution rates of players in this case.
     cur_payoff = 0;
     overall_coop_rate = sum(s);
-    %threshold_T = 1/3;
+    overal_coop = dot(evec,s);
+    
+    overall_coop_rate = overal_coop; % there are two methods to cosider 
+    % as a overall cooperation rate. If you want to use total rate of 
+    % cooperation instead of total endowments wich is contirubted, do 
+    % comment this line.
+    
+    threshold_T = 1/3;
     %threshold_T = 1/2;
     %threshold_T = 2/3;
-    threshold_T = 2.1;
+    %threshold_T = 2.1;
     delta = 10; % fixed parameter you can change it. power of sigmoid!
     our_way_of_payoff = 3; % choose your method here!
     % we have three way of having payoff for players.
